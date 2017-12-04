25e4b22e31b2bf1845c5a0eca8c77b97b7d44ce0
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-12-03 19:50:17 -0500

    button in position on splash

--- app/containers/Splash/index.js
+++ app/containers/Splash/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Background from './Background';
 import Button from '../../components/Button';
@@ -7,6 +8,11 @@ import Button from '../../components/Button';
 const wid = window.innerWidth;
 const hei = window.innerHeight;
 
+const ButtonStyle = styled.button`
+  position: absolute;
+  top: 5%;
+  left: 10%;
+`;
 class Splash extends React.Component {
   constructor(props) {
     super(props);
@@ -16,9 +22,11 @@ class Splash extends React.Component {
       <div>
         <Background width={`${wid}`} height={`${hei}`}>
         </Background>
-        <Button href={'/main'}>
-          SEE THIS?
-        </Button>
+        <ButtonStyle>
+          <Button href={'/main'}>
+            Enter here
+          </Button>
+        </ButtonStyle>
       </div>
 
     );

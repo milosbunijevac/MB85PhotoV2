f72ca9e6c23a050e4deb89358cd7e1922171dcc3
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-12-31 02:28:28 -0500

    got toolbar and titles showing, now i have to space them

--- app/components/NavBar/index.js
+++ app/components/NavBar/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import JssProvider from 'react-jss/lib/JssProvider';
+import createContext from '../styles/createContext';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const styles = {
+  root: {
+    width: '100%',
+  },
+};
 
 
 class NavBar extends React.Component {
+
   render() {
     return (
       <div>
-        This is the navbar component
+
+        <AppBar position="static" color="default" >
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              Models
+            </Typography>
+            <Typography type="title" color="inherit">
+              Adventures
+            </Typography>
+            <Typography type="title" color="inherit">
+              AboutMe
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
